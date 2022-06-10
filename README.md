72a44754224f2ed3fd0983a21acbe6a223c7aaff
raepple <martin@raepple.net>
2022-06-10 08:18:10 +0200

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # acrtest
+
+Simple Java app using vulnerable log4j version to test Azure Container Registry (ACR) image scanning with Defender for Cloud.
+
+Run mvn clean package to create the container image. Use docker push to upload the image to your ACR.
--- acrscantest/src/main/java/com/microsoft/samples/App.java
+++ acrscantest/src/main/java/com/microsoft/samples/App.java
@@ -1,5 +1,9 @@
 package com.microsoft.samples;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,6 +17,11 @@ public class App
 
     public static void main( String[] args )
     {
+        final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(App::generateLog, 0, 10, TimeUnit.SECONDS);
+    }
+
+    private static void generateLog() {
         logger.info( "Hello World!" );
     }
 }
